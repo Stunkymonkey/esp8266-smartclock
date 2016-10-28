6d31153765235cfc8fc56982a0ea7302a0efc2b3
friedrichweise <hi@weise.io>
2016-10-29 00:18:03 +0200

    add menu and socket inputs

--- server_base/ESP_clock/Webserver.ino
+++ server_base/ESP_clock/Webserver.ino
@@ -1,5 +1,8 @@
 void sendResponse(String content) {
-  String finalContent = "<!DOCTYPE html><html><head><title>" + deviceName + "</title></head><body><container>";
+  String finalContent = "<!DOCTYPE html><html><head><title>" + deviceName + "</title></head><body><nav>"+ deviceName +"<br>";
+  finalContent += "<ul><li><a href='/'>Home</a></li>";
+  finalContent += "<li><a href='/settings'>Settings</a></li></ul></nav><hr>";
+  finalContent += "<container>";
   finalContent += content;
   finalContent += "</container></body></html>";
   server.send(200, "text/html", finalContent);
@@ -8,21 +11,35 @@ void sendResponse(String content) {
 void createServer() {
   server.on("/", []() {
     content = "<h3>";
-    content += "Welcome";
-    content += "<br><a href='/settings'>Settings</a>";
+    content += "Welcome!";
     content += "</h3>";
     sendResponse(content);
   });
   server.on("/settings", []() {
+    //wifi settings
     content = "<h3>WiFi Settings</h3><form action='/wifiSet' method='GET'>";
     content += "<input type='text' name='ssid' placeholder='Your Wifi SSID' value='"+configSsid+"' autofocus><br>";
     content += "<input type='password' name='pw' placeholder='Your Wifi Password' value='"+configPw+"'><br>";
     content += "<input type='submit' value='Speichern'>";
     content += "</form>";
+    //name settings
     content += "<h3>Change Name</h3><form action='/nameSet' method='GET'>";
     content += "<input type='text' name='name' placeholder='Your Device Name' value='"+deviceName+"' autofocus><br>";
     content += "<input type='submit' value='Speichern'>";
     content += "</form>";
+    //sockets
+    content += "<h3>Sockets</h3>";
+    for(int i=0; i<3; i++) {
+      content += "<form action='/socketSet' method='GET'><span>Socket ";
+      content +=  String(i);
+      content += "</span><br>";
+      content += "<input type='text' name='name' placeholder='Title for socket'>";
+      content += "<input type='text' name='houseCode' placeholder='Housecode'>";
+      content += "<input type='number' name='groupCode' placeholder='Gruppe (optional)'>";
+      content += "<input type='number' name='socketCode' placeholder='Device Code'>";
+      content += "<input type='submit' value='Speichern'>";
+      content += "</form><br>";
+    }
     sendResponse(content);
   });
   server.on("/wifiSet", []() {
