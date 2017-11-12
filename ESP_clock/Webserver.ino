a4dde344904068b9805c45325614c1ded7ce181c
Felix Buehler <account@buehler.rocks>
2017-11-12 23:16:11 +0100

    first try to have new website

--- ESP_clock/Webserver.ino
+++ ESP_clock/Webserver.ino
@@ -50,12 +50,19 @@ void sendResponse(String content) {
   WifiLEDOn();
   String finalContent = "<!DOCTYPE html><html><head><title>" + deviceName + "</title><meta name='viewport' content='initial-scale=1'><style>";
   finalContent += "container,input,nav{border-radius:2px;box-sizing:border-box}.switch,container,input,nav{box-sizing:border-box}body{background:#1F2237;color:#f9f9f9;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen,Ubuntu,Cantarell,\"Fira Sans\",\"Droid Sans\",\"Helvetica Neue\",sans-serif;font-size:16px;margin:0}a{color:inherit}container,nav{color:#1F2237;background:#f9f9f9;width:95%;max-width:800px;display:block;margin:1em auto;padding:.5em 1em}nav{display:flex;align-items:baseline;margin-top:0}nav h2{flex-grow:2}nav ul{padding:0;list-style:none}nav li{display:inline-block;margin-right:1em}input{padding:1em .8em;margin-bottom:.8em;margin-right:.5em;width:100%;max-width:300px;border:1px solid gray;outline:0;font-size:.8rem}input[type=submit]{background-color:#64a97c;color:#fff;border-color:#64a97c}label{display:block;margin-bottom:.2em;font-size:.9em;color:#656565}#l0{color: #008000;}#l1{color: #FFA500;}#l2{color: #FF8040;}#l3{color: #FF0000;}#l4{color: #800000;}.switch{display:inline-block;padding:1.5em 0;width:30%;background:#cc5e5e;text-align:center;margin-right:5%;color:#fff;font-size:1.4em;font-weight:700;text-decoration:none;border-radius:3px}.switch:last-child{margin-right:0}.sensorData{display:flex;justify-content:space-around}";
-  finalContent += "</style></head><body><nav><h2>"+ deviceName + "</h2>";
-  finalContent += "<ul><li><a href='/'>Home</a></li>";
-  finalContent += "<li><a href='/settings'>Settings</a></li></ul></nav>";
-  finalContent += "<container>";
+  finalContent += "</style>";
+  finalContent += "</head><body><nav><container class=\"navContainer\"><h2>"+ deviceName + "</h2>";
+  if (ENABLE_SENSOR) {
+    finalContent += "<div class=\"sensorConainer\">";
+    finalContent += "<span>" + String(temperature) + "&deg;C</span><span>" + String(humidity) + "%</span><spanid='l" + String(get_heat_index_level()) + "'>" + String(heatindex) + "HT</span>";
+    finalContent += "</div>";
+  }
+  finalContent += "<a href=\"turnLEDOFF\" class=\"bulbIcon\"></a>";
+  finalContent += "<a href=\"/settings\" class=\"settingsIcon\"></a>";
+  finalContent += "</container>";
+  finalContent += "</nav>";
   finalContent += content;
-  finalContent += "</container></body></html>";
+  finalContent += "</body></html>";
   server.send(200, "text/html", finalContent);
   WifiLEDOff();
 }
@@ -67,20 +74,34 @@ void sendResponse(String content) {
 void Home() {
   if (!auth()) { return; }
   content = "";
+  content += "<container>";
   if (ENABLE_SOCKETS) {
-    content += "<h3>Control your home:</h3>";
+    content += "<div class=\"switchContainer\">";
+    content += "<h3>Master</h3>";
+    content += "<div class=\"switch\">";
+    content += "<a href=\"#\" class=\"onLabel\">ON</a>";
+    content += "<a href=\"#\" class=\"offLabel\">OFF</a>";
+    content += "<div class=\"toggler\"></div>";
+    content += "</div>";
     for(int i=0; i<(sizeof configSocketSets / sizeof configSocketSets[0]); i++) {
-      content += "<p><label class='title'>" + configSocketSets[i][1] + "</label><a class='switch' href=\"socket" + String(i) + "On\">ON</a><a class='switch' href=\"socket" + String(i) + "Off\">OFF</a><a class='switch' href=\"socket" + String(i) + "Toggle\">Toggle</a></p>";
+      content += "<div class=\"switchContainer small\">";
+      content += "<h4>" + configSocketSets[i][1] + "</h4>";
+      content += "<div class=\"switch ";
+      if(statusSocketSets[i]){
+        content += "on";
+      } else {
+        content += "off";
+      }
+      content += ">";
+      content += "<a href=\"socket" + String(i) + "On\" class=\"onLabel\">ON</a>";
+      content += "<a href=\"socket" + String(i) + "Off\" class=\"offLabel\">OFF</a>";
+      //content += "<a href=\"socket" + String(i) + "Toggle\" class=\"toggleLabel\">ASDF</a>";
+      content += "<div class=\"toggler\"></div>";
+      content += "</div>";
+      content += "</div>";
     }
   }
-  if (ENABLE_SENSOR) {
-    content += "<h3>Sensor information</h3>";
-    content += "<div class=\"sensorData\">";
-    content += "<p><label class='title'>Temperature:</label>" + String(temperature) + " &deg;C</p>";
-    content += "<p><label class='title'>Humidity:</label>" + String(humidity) + " %</p>";
-    content += "<p id='l" + String(get_heat_index_level()) + "'><label class='title'>Heat-Index:</label>" + String(heatindex) + "</p>";
-    content += "</div>";
-  }
+  content += "</div>";
   if (ENABLE_MATRIX) {
     content += "<h3>LED Matrix</h3>";
     content += "<p><a class='switch' href=\"ledsOn\">ON</a><a class='switch' href=\"ledsOff\">OFF</a><a class='switch' href=\"ledsToggle\">Toggle</a></p>";
@@ -88,6 +109,7 @@ void Home() {
   if (DEBUG_STRING != "") {
     content += "<h3>" + DEBUG_STRING + "</h3>";
   }
+  content += "</container>";
   sendResponse(content);
 }
 
@@ -96,9 +118,11 @@ void Home() {
  * markup for the settings page
  */
 void Settings() {
+  content = "";
+  content += "<container class=\"small\">";
   if (!auth()) { return; }
   //wifi settings
-  content = "<h3>WiFi Settings</h3><form action='/wifiSet' method='GET'>";
+  content += "<h3>WiFi Settings</h3><form action='/wifiSet' method='GET'>";
   content += "<label for='ssid'>Wifi SSID</label><input id='ssid' type='text' name='ssid' placeholder='Your Wifi SSID' value='"+WifiSsid+"' autofocus><br>";
   content += "<label for='pw'>Wifi Password</label><input id='pw' type='password' name='pw' placeholder='Your Wifi Password'><br>";
   content += "<input type='submit' value='Speichern'>";
@@ -146,6 +170,7 @@ void Settings() {
   content += "<h3>RESTART</h3><form action='/restart' method='GET'>";
   content += "<input type='submit' value='RESTART'>";
   content += "</form>";
+  content += "</container>";
   sendResponse(content);
 }
 
