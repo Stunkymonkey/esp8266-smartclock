6bfe83d1176147e0f5feebd96a66549661e219bc
friedrichweise <account@weise.io>
2018-05-28 21:34:46 +0200

    #21 add comments for VE.Direct fields

--- ESP_clock/victron.h
+++ ESP_clock/victron.h
@@ -31,22 +31,23 @@
   "HSDS",
   "Checksum"
   };
-  #define PID 0
-  #define FW 1
-  #define SER 2 // Offically SER# but # does not play that well as macro
-  #define V 3     // ScV
-  #define I 4     // ScI
-  #define VPV 5   // PVV
-  #define PPV 6   // PVI = PVV / VPV
-  #define CS 7    // ScS
-  #define ERR 8   // ScERR
-  #define LOAD 9  // SLs
-  #define IL 10   // SLI
-  #define H19 11
-  #define H20 12
-  #define H21 13
-  #define H22 14
-  #define H23 15
-  #define HSDS 16
+  // source: VE.Direct Protocol Whitepaper 3.24
+  #define PID 0 //Product ID
+  #define FW 1 //Firmware Version
+  #define SER 2 //Serial number (SER#)
+  #define V 3     // Battery Voltage in mV
+  #define I 4     // Battery Current in mA
+  #define VPV 5   // Panel Voltage in mV
+  #define PPV 6   // Panel Power in Watt
+  #define CS 7    // State of Operation (0-9: whitepaper for reference)
+  #define ERR 8   // Error code (0-116: whitepaper for reference)
+  #define LOAD 9  // Load output State (ON/OFF)
+  #define IL 10   // Load current in mA
+  #define H19 11  // Yield total (user resettable counter) in 0.01 kWh
+  #define H20 12 // Yield today in 0.01 kWh
+  #define H21 13 // Maximum Power today in W
+  #define H22 14 // Yield yesterday in 0.01 kWh
+  #define H23 15 // Maximum Power yesterday in W
+  #define HSDS 16 //Day sequence number (0..364)
   #define CHECKSUM 17
 #endif
