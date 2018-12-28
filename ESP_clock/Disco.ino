57c932424a9b18a15afcfdef91016726907a93cf
friedrichweise <account@weise.io>
2018-12-28 16:14:59 +0100

    first swimming fish

--- ESP_clock/Disco.ino
+++ ESP_clock/Disco.ino
@@ -36,22 +36,41 @@ void random_pixel() {
   lc.setLed(addr, index, pos, state);
 }
 
+
 /*
- * cosine wave
+ * waterworld
  */
-void cosine_wave() {
-  clearMatrix();
-  if(DiscoTmp==32) {
-    DiscoTmp = 0;
-  }
-  for(int k = 0; k<lc.getDeviceCount(); k++) {
-    for(int i = 0; i<8; i++) {
-      int x = (k*8)+i;
-      int y = (int)(4*cos((float)x*0.3+DiscoTmp)+4+0.5);
-      for(int a = 0; a<y; a++) {
-        lc.setLed(k, a, 7-i, true);
+ byte fishes[1][8] = {
+  {B00010000,B00111000,B01000100,B01000100,B01000100,B00111000,B00010000,B00111000}, // medium fish
+};
+const long WATERWORLD_SPEED = 200;
+ void waterworld() {
+    static unsigned long lastWaterworldDraw;
+    unsigned long currentDraw = millis();
+    if(currentDraw - lastWaterworldDraw >= WATERWORLD_SPEED || lastWaterworldDraw == 0) {
+      lastWaterworldDraw = currentDraw;
+      clearMatrix();
+      drawWaterWorld();
+    }
+  
+ }
+
+int iconPosition = lc.getDeviceCount() * 8;
+void drawWaterWorld() {
+    int amount = lc.getDeviceCount() * 8;
+    int currentRow = 0;
+    for(int k = 0; k<lc.getDeviceCount(); k++) {
+      for(int i=0; i<8; i++) {
+        currentRow++;
+        //currentRow is in the iconPosition Window
+        if(currentRow >= iconPosition && currentRow < (iconPosition+8)) {
+          print(currentRow);
+          int rowInIconPosition = currentRow - iconPosition;
+          //displayColumn(k, i, fishes[0][rowInIconPosition]);
+           displayRow(k, i, fishes[0][rowInIconPosition]);
+        } 
       }
     }
-  }
-  DiscoTmp++;
+    iconPosition = iconPosition-1;
+    if (iconPosition < -8) iconPosition = lc.getDeviceCount() * 8;
 }
