fb6a1ab47a348ca2a80127d46c7f902650bc48f8
Felix Buehler <account@buehler.rocks>
2020-04-05 19:36:28 +0200

    [time] calculate DST differently

--- ESP_clock/Time.ino
+++ ESP_clock/Time.ino
@@ -1,39 +1,15 @@
 /*
    checks if we are in summer time
+   source: https://github.com/gmag11/NtpClient/blob/master/src/NTPClientLib.cpp
 */
-bool inSummerTime(int hours, int currentDay, int currentMonth, int currentYear) {
-  if (currentDay < 0 || currentMonth < 0 || currentYear < 0) return false;
-  if ((currentMonth >= 3) && (currentMonth <= 10)) { // March to October inclusive
-    if ((currentMonth > 3) && (currentMonth < 10)) { // Definitely
-      return true;
-    }
-    if (currentMonth == 3) { // Have we passed the last SuncurrentDay of March, 1am UT ?
-      if (currentDay >= 25) { // It's getting interesting
-        int dw = dayOfWeek(now()) - 1;
-        // When is the next suncurrentDay ?
-        int dts = 6 - dw; // Number of currentDays before the next suncurrentDay
-        if (dts == 0) dts = 7; // We are really speaking of the next suncurrentDay, not the current one
-        if ((currentDay + dts) > 31) { // The next suncurrentDay is next currentMonth !
-          if (dw != 6 || hours > 0) // We finally check that we are not on the currentDay of the change before the time of the change
-            return true;
-        }
-      }
-    }
-    if (currentMonth == 10) { // Have we passed the last SuncurrentDay of October 1am UT ?
-      if (currentDay >= 25) { // It's getting interesting
-        int dw = dayOfWeek(now()) - 1;
-        // When is the next suncurrentDay ?
-        int dts = 6 - dw; // Number of currentDays before the next suncurrentDay
-        if (dts == 0) dts = 7; // We are really speaking of the next suncurrentDay, not the current one
-        if ((currentDay + dts) > 31) { // The next suncurrentDay is next currentMonth !
-          if (dw != 6 || hours > 0) // We finally check that we are not on the currentDay of the change before the time of the change
-            return false; // We have passed the change
-          else return true;
-        } else return true;
-      } else return true;
-    }
-  }
-  return false;
+
+bool isSummerTime(int hour, int day, int month, int year, int tzHours) {
+  if ((month < 3) || (month > 10)) return false; // no summertime in Jan, Feb, Nov, Dez
+  if ((month > 3) && (month < 10)) return true; // summertime in Apr, May, Jun, Jul, Aug, Sep
+  if ((month == 3 && ((hour + 24 * day) >= (1 + tzHours + 24 * (31 - (5 * year / 4 + 4) % 7)))) || ((month == 10 && (hour + 24 * day) < (1 + tzHours + 24 * (31 - (5 * year / 4 + 1) % 7)))))
+    return true;
+  else
+    return false;
 }
 
 /*
